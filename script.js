e952bdbe7feee74d978cf3f9276a97eb3ce1c312
infoinmovie <infoinmovie.official@gmail.com>
2025-03-01 08:58:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("playPauseBtn");
 let isPlaying = false;
-const text = "Info in Movie News. Tom Hardy is once again displaying his prowess in the thrilling movie HAVOC, proving himself as one of Hollywood's toughest actors. Known for his intense and physical roles, he is prepared to draw viewers into a world filled with intrigue and violence. According to The Hollywood Reporter, HAVOC follows the story of Walker (played by Hardy), a detective who must survive pursuit from criminal gangs, corrupt politicians, and treasonous police following a chaotic drug deal. In his effort to rescue a politician's child, Walker fights against not just enemies, but also faces his dark past.";
+const text = "This is a sample text-to-speech audio. Welcome to our article!";
 
 // Create speech synthesis
 const speech = new SpeechSynthesisUtterance(text);
